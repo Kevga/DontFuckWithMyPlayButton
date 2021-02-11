42cc50da42e3a117fd2b0ddd0cbe073e2038b5aa
Kevin Galitzki <dakari91@gmail.com>
2021-02-11 19:51:36 +0100

    Initial commit

---
+++ DontFuckWithMyPlayButton/Patches/ScoreSaberPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace DontFuckWithMyPlayButton.Patches
+{
+	[HarmonyPatch(typeof(ScoreSaber.UI.Other.ScoreSaberLeaderboardView), "SetPlayButtonState")]
+	[UsedImplicitly]
+	public class ScoreSaberPatch
+	{
+		[UsedImplicitly]
+		// ReSharper disable once InconsistentNaming
+		// ReSharper disable once RedundantAssignment
+		private static void Prefix(ref bool state)
+		{
+			state = true;
+		}
+	}
+}
---
+++ DontFuckWithMyPlayButton/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using IPA.Logging;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace DontFuckWithMyPlayButton
+{
+	[Plugin(RuntimeOptions.DynamicInit)]
+	public class Plugin
+	{
+		private const string HARMONY_ID = "com.github.kevga.playbutton";
+		private Harmony _harmonyInstance = null!;
+		public static Logger Logger = null!;
+
+		[Init]
+		public void Init(Logger ipaLogger)
+		{
+			Logger = ipaLogger;
+		}
+
+		[OnEnable]
+		public void OnEnable()
+		{
+			_harmonyInstance = new Harmony(HARMONY_ID);
+			_harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[OnDisable]
+		public void OnDisable()
+		{
+			_harmonyInstance.UnpatchAll(HARMONY_ID);
+		}
+	}
+}
